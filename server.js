38241eaceee64f75d727006fc64fe5fccf1bc39c
Ashwin Goswami <ashwin200459@gmail.com>
2026-04-07 01:09:17 +0530

    Serve static files from the 'public' directory

--- server.js
+++ server.js
@@ -100,3 +100,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log("Server running on port " + PORT);
 });
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
